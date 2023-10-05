c5ba787039639ae16a43dc8b06871f31d1baaf56
sofierooed <sofierooed@gmail.com>
2023-10-04 14:46:29 -1000

    WOD SPP2

---
+++ WODs/SmartPhoneProducts2/products_data.js
@@ -0,0 +1,21 @@
+console.log("Product variables loaded...");
+
+let name1 = "HTC";
+let price1 = 40.00;
+let image1 = "./images/HTC.jpg";
+
+let name2 = "Apple";
+let price2 = 75.00;
+let image2 = "./images/iphone-3gs.jpg";
+
+let name3 = "Nokia";
+let price3 = 35.00;
+let image3 = "./images/Nokia.jpg";
+
+let name4 = "Samsung";
+let price4 = 45.00;
+let image4 = "./images/Samsung.jpg";
+
+let name5 = "Blackberry";
+let price5 = 10.00;
+let image5 = "./images/Blackberry.jpg";
