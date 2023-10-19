0db4e4649c2f9ca6420a3e5edb200c3e40b5fc1d
sofierooed <sofierooed@gmail.com>
2023-10-19 11:48:07 -1000

    Lab10

---
+++ Lab10/Ex1.js
@@ -0,0 +1,15 @@
+year = 2001;
+day = 11;
+month = 7;
+step1 = 1;
+step2 = parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 6;
+step6 = step3 + step4;
+step7 = step6 + day;
+step8 = step7;
+step9 = step8 - 1;
+final = step9 % 7;
+
+
+console.log(final);
