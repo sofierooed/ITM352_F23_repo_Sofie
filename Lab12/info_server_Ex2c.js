bb852ab1de7336329558aaa5fb0a64393e5baeaf
sofierooed <sofierooed@gmail.com>
2023-11-07 11:48:40 -1000

    Lab12

--- Lab12/info_server_Ex2c.js
+++ Lab12/info_server_Ex2c.js
@@ -10,6 +10,7 @@ app.get('/test', function (request, response, next) {
     response.send('in route GET to /test');
 });
 
+//Middleware
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
---
+++ Lab12/public/products_data.js
@@ -0,0 +1,14 @@
+console.log("Product variables loaded...");
+
+product1 = {image: "./images/HTC.jpg", brand: "HTC", price: 40.00};
+
+product2 = {image: "./images/iphone-3gs.jpg", brand: "Apple", price: 75.00};
+
+product3 = {image: "./images/Nokia.jpg", brand: "Nokia", price: 35.00};
+
+product4 = {image: "./images/Samsung.jpg", brand: "Samsung", price: 45.00};
+
+product5 = {image: "./images/Blackberry.jpg", brand: "Blackberry", price: 10.00};
+
+
+products = [product1, product2, product3, product4, product5]
