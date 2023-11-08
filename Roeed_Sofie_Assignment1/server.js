1f43227110c7842e019ee10a095da846c4daed46
sofierooed <sofierooed@gmail.com>
2023-11-07 22:12:44 -1000

    Assignment1 start

---
+++ Roeed_Sofie_Assignment1/server.js
@@ -0,0 +1,34 @@
+//Load product data
+const products_array = require(__dirname + '/product_data.json');
+
+const express = require('express');
+const app = express();
+
+// Function to check if quantities entered are (1) whole numbers, (2) not negative, and (3) a number and not a word or character; taken from previous labs
+function isNonNegInt(quantities, returnErrors) {
+   errors = []; // assume no errors at first
+   if (Number(quantities) != quantities) errors.push(' Not a number'); // Check if string is a number value
+   if (quantities < 0) errors.push(' Negative value'); // Check if it is non-negative
+   if (parseInt(quantities) != quantities) errors.push(' Not an integer'); // Check that it is an integer
+
+
+   var returnErrors = returnErrors ? errors : (errors.length == 0);
+   return (returnErrors);
+};
+
+// Routing 
+
+// monitor all requests regardless of method and path
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+// process purchase request (validate quantities, check quantity available)
+<** your code here ***>
+
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
+
+// start server
+app.listen(8080, () => console.log(`listening on port 8080`));
