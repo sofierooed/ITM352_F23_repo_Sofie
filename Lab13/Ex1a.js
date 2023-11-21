428e125333f81b918e912c1c7624a88f1e9ea47b
sofierooed <sofierooed@gmail.com>
2023-11-21 13:43:47 -1000

    Lab13

---
+++ Lab13/Ex1a.js
@@ -0,0 +1,6 @@
+let user_data_filename = 'user_data.json';
+let users_reg_data = require("./user_data.json");
+console.log(users_reg_data["kazman"]["password"]);
+
+
+
---
+++ Lab13/Ex1b.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+let user_data_filename = 'user_data.json';
+let user_reg_data_JSON = fs.readFileSync(user_data_filename, 'utf-8');
+let users_reg_data = JSON.parse(user_reg_data_JSON);
+
+//console.log(user_reg_data_JSON);
+console.log(users_reg_data["kazman"]["password"]);
